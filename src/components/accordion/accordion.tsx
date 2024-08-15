295053ded6e17dcad2eefdf75baf4c32976eeb3e
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2024-08-15 18:47:05 +0300

    rewrite accordions using details tag

--- src/components/accordion/accordion.tsx
+++ src/components/accordion/accordion.tsx
@@ -1,47 +1,21 @@
-"use client";
-
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import * as AccordionPrimitive from "@radix-ui/react-accordion";
-import * as React from "react";
 
-const Accordion = AccordionPrimitive.Root;
+export function AccordionItem({ children }: { children: React.ReactNode }) {
+  return <details className="group">{children}</details>;
+}
 
-const AccordionItem = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
->(({ ...props }, ref) => <AccordionPrimitive.Item ref={ref} {...props} />);
-AccordionItem.displayName = "AccordionItem";
-
-const AccordionTrigger = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ children, ...props }, ref) => (
-  <AccordionPrimitive.Header className="flex">
-    <AccordionPrimitive.Trigger
-      ref={ref}
-      className="mt-2 flex w-96 flex-1 items-center justify-between gap-3 border-b border-purple-600 px-8 py-6 text-left text-base font-bold leading-tight text-neutral-950 transition-transform hover:underline [&[data-state=open]>svg]:rotate-180 [&[data-state=open]]:border-b-0 [&[data-state=open]]:bg-gray-100"
-      {...props}
+export function AccordionTrigger({ children }: { children: React.ReactNode }) {
+  return (
+    <summary
+      role="button"
+      className="mt-2 flex w-full items-center justify-between gap-3 border-b border-purple-600 px-8 py-6 text-base font-bold leading-tight hover:underline group-open:bg-gray-100"
     >
-      {children}
-      <ChevronDownIcon className="h-6 w-6 shrink-0 stroke-2 text-purple-600 transition-transform duration-200" />
-    </AccordionPrimitive.Trigger>
-  </AccordionPrimitive.Header>
-));
-AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
+      <div>{children}</div>
+      <ChevronDownIcon className="h-6 w-6 shrink-0 stroke-2 text-purple-600 transition-transform duration-200 group-open:rotate-180" />
+    </summary>
+  );
+}
 
-const AccordionContent = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
->(({ children, ...props }, ref) => (
-  <AccordionPrimitive.Content
-    ref={ref}
-    className="overflow-hidden px-8 transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
-    {...props}
-  >
-    <div className="py-8">{children}</div>
-  </AccordionPrimitive.Content>
-));
-
-AccordionContent.displayName = AccordionPrimitive.Content.displayName;
-
-export { Accordion, AccordionContent, AccordionItem, AccordionTrigger };
+export function AccordionContent({ children }: { children: React.ReactNode }) {
+  return <div className="overflow-hidden px-8 py-8">{children}</div>;
+}
--- src/examples/accordion-example.tsx
+++ src/examples/accordion-example.tsx
@@ -1,5 +1,4 @@
 import {
-  Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
@@ -36,28 +35,69 @@ export default function AccordionExample() {
         <div className="lg:w-72"></div>
 
         <div className="flex-1">
-          <Accordion type="multiple">
-            {accordionItems.map((item, index) => (
-              <AccordionItem
-                value={index + item.title}
-                key={index + item.title}
-              >
-                <AccordionTrigger>{item.title}</AccordionTrigger>
-                <AccordionContent>{item.body}</AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
+          <AccordionItem>
+            <AccordionTrigger>Title content</AccordionTrigger>
+            <AccordionContent>
+              <p className="mb-4">
+                Body content. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
+              </p>
+              <p>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est
+                laborum.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionTrigger>Second title</AccordionTrigger>
+            <AccordionContent>
+              <p className="mb-4">
+                Body content. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
+              </p>
+              <p>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est
+                laborum.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionTrigger>Third title</AccordionTrigger>
+            <AccordionContent>
+              <p className="mb-4">
+                Body content. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
+              </p>
+              <p>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est
+                laborum.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
         </div>
       </div>
       <CodeBlock>
         <CodeHighlight title="accordion-example.tsx">
           {`
-<Accordion type="multiple">
-  <AccordionItem value="item-1">
-    <AccordionTrigger>Title content</AccordionTrigger>
-    <AccordionContent>Body content</AccordionContent>
-  </AccordionItem>
-</Accordion>
+<AccordionItem>
+  <AccordionTrigger>Title content</AccordionTrigger>
+  <AccordionContent>Body content</AccordionContent>
+</AccordionItem>
               `}
         </CodeHighlight>
       </CodeBlock>

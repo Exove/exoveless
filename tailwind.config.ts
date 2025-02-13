327acda6ddc39bbf10e96c92de851c1d3fa81385
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-02-13 23:12:04 +0200

    init main menu example

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,22 +8,9 @@ const config = {
       fontFamily: {
         sans: ["var(--font-montserrat)"],
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      colors: {},
     },
   },
-  plugins: [typography, headlessui],
+  plugins: [typography, headlessui, require("tailwindcss-animate")],
 };
 export default config;

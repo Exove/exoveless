10ccd4b59a9b1074430b39a98889084d2a731926
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-11-24 20:29:36 +0200

    remove extra stuff

--- src/components/containers/button-container.tsx
+++
@@ -1,7 +0,0 @@
-interface ButtonContainerProps {
-  children: React.ReactNode;
-}
-
-export default function ButtonContainer({ children }: ButtonContainerProps) {
-  return <div className="flex flex-wrap items-center gap-8">{children}</div>;
-}
--- src/components/containers/content-section.tsx
+++
@@ -1,7 +0,0 @@
-interface ContentSectionProps {
-  children: React.ReactNode;
-}
-
-export default function ContentSection({ children }: ContentSectionProps) {
-  return <div className="my-16 ">{children}</div>;
-}
